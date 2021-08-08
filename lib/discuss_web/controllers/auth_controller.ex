22457802f9568b7bcc83cbef37158a4c16a847d3
Joao Rodrigues <joaocarlos86@gmail.com>
2021-08-08 18:02:50 +0100

    Adding authentication with github

---
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -0,0 +1,36 @@
+defmodule DiscussWeb.AuthController do
+  use DiscussWeb, :controller
+
+  plug Ueberauth
+
+  alias Discuss.{User, Repo}
+
+  def callback(conn, params) do
+    %{assigns: %{ueberauth_auth: auth}} = conn
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil -> Repo.insert(changeset)
+      user -> {:ok, user}
+    end
+  end
+end
