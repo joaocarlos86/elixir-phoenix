cbcad63a49dd1254b05d62e8e9355665191c87b3
Joao Rodrigues <joaocarlos86@gmail.com>
2021-08-11 18:26:35 +0100

    Adding Plug to check if the user is authenticated

--- lib/discuss_web/controllers/plugs/set_user.ex
+++ lib/discuss_web/controllers/plugs/set_user.ex
@@ -1,15 +1,13 @@
 defmodule DiscussWeb.Plugs.SetUser do
   import Plug.Conn
-  import Phoenix.Controller
 
   alias Discuss.{Repo, User}
-  alias DiscussWeb.Router.Helpers
 
   def init(_params) do
 
   end
 
-  def call(conn, _params) do
+  def call(conn, _paramsFromInitFunction) do
     user_id = get_session(conn, :user_id)
 
     cond do
