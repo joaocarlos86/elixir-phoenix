c41a78a34b7126ddbcb379dd79f18041937c8a50
Joao Rodrigues <joaocarlos86@gmail.com>
2021-08-15 17:31:01 +0100

    Using elixir tokens to identify the user that is opening the socket

--- lib/discuss_web/channels/user_socket.ex
+++ lib/discuss_web/channels/user_socket.ex
@@ -4,8 +4,16 @@ defmodule DiscussWeb.UserSocket do
   channel "comments:*", DiscussWeb.CommentsChannel
 
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(params, socket, _connect_info) do
+    %{"token" => token} = params
+
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _error} ->
+        :error
+    end
+
   end
 
   @impl true
